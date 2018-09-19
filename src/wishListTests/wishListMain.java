de5815d341b99eee0097b4601cae8f0bbc8ec3f2
TL <github@littleoptions.co.nz>
2018-09-20 11:10:43 +1200

    Update to simplified version

--- src/wishListTests/wishListMain.java
+++ src/wishListTests/wishListMain.java
@@ -5,35 +5,30 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 //comment the line above and uncomment line below to use Chrome
 //import org.openqa.selenium.chrome.ChromeDriver;
 
-import wishlisttest.searchPage;
-import wishlisttest.productPage;
-import wishlisttest.wishListPage;
+import wishListPages.pageObjects;
+import wishListPages.testData;
+
 
 public class wishListMain {
 
-	 private static WebDriver driver = null;
-	 
 	 public static void main(String[] args) {
-	       // declaration and instantiation of objects/variables
-		  
-		   // Uncomment the line below if using Firefox and Gecko is not in you system path.  Change path to the location of your Gecko install.
-		   // System.setProperty("webdriver.firefox.marionette","C:\\Users\\OEM\\Documents\\Selenium\\gecko\\geckodriver.exe");
-		   
-		   // Suppress logs with warning
+		       // declaration and instantiation of objects/variables
+			  
+			   // Uncomment the line below if using Firefox and Gecko is not in your system path.  Change path to the location of your Gecko install.
+			   // System.setProperty("webdriver.firefox.marionette","C:\\Users\\OEM\\Documents\\Selenium\\gecko\\geckodriver.exe");
+			   
+			   // Suppress logs with warning
 		   System.setProperty(FirefoxDriver.SystemProperty.BROWSER_LOGFILE,"/dev/null");
 		   
-		   driver = new FirefoxDriver();
-	       
-		   // Change baseurl below to change between environments, e.g. Test, Staging, or Production 
-	        String baseUrl = "https://www.bunnings.com.au/search/products?q=paint&redirectFrom=Any";
-		   	        
-	        // launch browser and direct it to the Base URL
-	        driver.get(baseUrl);
+		   WebDriver driver = new FirefoxDriver();          
+   	        
+	        	// launch browser and direct it to the Base URL
+	        driver.get(testData.baseUrl);
 
-	        // Click on a random product and go to product page
-	        searchPage.clickOnProd(driver).click();
+	        	// Click on a product and go to product page
+	        pageObjects.clickOnElment(driver, testData.prodToClick);
 	        
-	        // Wait for page to load
+	        	// Wait for page to load
 	    	 
 	    	try {
 				Thread.sleep(4000);
@@ -41,10 +36,10 @@ public class wishListMain {
 				e.printStackTrace();
 			}
 	        
-	         // Add product to Wish List
-	    	productPage.addToWishList(driver).click();
+	         	// Add product to Wish List
+	        pageObjects.clickOnElment(driver, testData.addToList);
 
-	        // Wait for page to load
+	        	// Wait for page to load
     	 
 	    	try {
 				Thread.sleep(3000);
@@ -52,22 +47,20 @@ public class wishListMain {
 				e.printStackTrace();
 			}
 	         
-	    	// Click Wish List link and go to Wish List page
-	    	wishListPage.checkWishList(driver).click();
-	              
-	         /*
-	          * compare the wish list with the expected one and print
-	          * the result as "Passed" or "Failed"
-	         */
-	         if (wishListPage.verifyWishList(driver) != null){
-	             System.out.println("Test Passed!");
+	    		// Click Wish List link and go to Wish List page
+	        pageObjects.clickOnElment(driver, testData.goToWishList);
+    
+	         	// See if a value is returned and print result as "Passed" or "Failed"
+	         
+	         if (pageObjects.returnElement(driver, testData.prodName) != null){
+	             System.out.printf("Test Passed! ");
 	         } else {
 	             System.out.println("Test Failed");
 	         }   
 	         
-	         //close browser
+	         	//close browser
 	         driver.close();
 	       
-	   }  
-	    
+	   }
+  
 }
